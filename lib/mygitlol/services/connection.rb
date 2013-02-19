10fa48a1b23297ebbc4e37314671da8dd3b2699c
Scott Helm <helm.scott@gmail.com>
2013-02-18 22:15:34 -0500

    adds lib folder

---
+++ lib/mygitlol/services/connection.rb
@@ -0,0 +1,25 @@
+require 'base64'
+
+module MyGitLol
+  module Services
+    class Connection
+      attr_accessor :base_url, :auth_header, :path, :options, :access_token
+
+      def get
+
+      end
+
+      def post
+
+      end
+
+      def put
+
+      end
+
+      def delete
+
+      end
+    end
+  end
+end
